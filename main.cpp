38d858b75922fcb47b82d34bd575d876eb681cbf
Mostafamahdy1 <Mostafaeltayebbb40@gmail.com>
2022-02-28 14:51:19 +0000

    arrays revision

--- main.cpp
+++ main.cpp
@@ -1,19 +1,14 @@
+#include <array>
 #include <cstdio>
 
 int main()
 {
-    std::printf("Enter a list of numbers (0 to end): ");
-    
-    
-
-    while (true)
-    {
-        std::scanf("%d", &num);
-        if (num == 0)
+        std::array<int, 4> arr1 = {1, 2, 3, 4};
+        std::array<int, 4> arr2 = {5, 6, 7, 8};
+        int  sum  = 0;
+        for (int i = 0; i < arr1.size(); i += 1)
         {
-            break;
+                sum = arr1[i] + arr2[i];
+                std::printf("Sum[%d}=%d\n", i, sum);
         }
-        
-    }
-    
 }
